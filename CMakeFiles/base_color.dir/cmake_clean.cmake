923dc2b22442be6c893c20b751a4bee8e04e9f34
validovsergei <validovsergei@gmail.com>
2021-12-09 08:49:55 +0300

    commit 1 from 09.12.21

---
+++ CMakeFiles/base_color.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/base_color.dir/main.cpp.o"
+  "base_color"
+  "base_color.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base_color.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
